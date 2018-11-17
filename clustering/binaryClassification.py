df24d5b5b85830efd7af37062ea306f33d0f03ff
cloudsTrafton <traftonator@gmail.com>
2018-11-17 14:04:49 -0500

    Updated data utils, processed dataset, preliminary binary clustering

---
+++ clustering/binaryClassification.py
@@ -0,0 +1,52 @@
+import scipy
+from sklearn.cluster import KMeans
+import data_processing.dataUtils as dataUtils
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+# Binary Clustering for age.
+# Reads in the dataset and performs binary clustering, i.e. creates two clusters:
+# First cluster is all data that is calculated to belong in the specified age range
+# Second cluster is all those data points determined not to belong in the specified age range
+# These clusters are built using the K-Means algorithm, which is implemented by the sklearn library
+
+#NOTE: thank you to StackExchange for guidance on how to use SKLearn KMeans:
+#https://datascience.stackexchange.com/questions/16700/confused-about-how-to-apply-kmeans-on-my-a-dataset-with-features-extracted
+
+
+
+
+#Runs the K-Means clustering algorithm for binary clustering using SKLearn's optimized centroid placement
+
+# -- Preliminary Trial for Clustering data based on the three features extracted as N-Grams -- #
+
+nGramData = dataUtils.retreiveDataSet('../feature_sets/n_grams.csv')
+nGramDataCopy = nGramData
+
+# Parameter:
+# init: k-means++ -
+kmeans = KMeans(init='k-means++', n_clusters=2, n_init=10)
+result = kmeans.fit(nGramData)
+labels = kmeans.predict(nGramData)
+for label in labels:
+    print(label)
+
+#dataUtils.plotData(labels)
+
+# in row_dict we store actual meanings of rows, in my case it's russian words
+# clusters = {}
+# n = 0
+# for item in labels:
+#     if item in clusters:
+#         clusters[item].append(row_dict[n])
+#     else:
+#         clusters[item] = [row_dict[n]]
+#         n +=1
+#
+# for item in clusters:
+#     print ("Cluster ", item)
+#     for i in clusters[item]:
+#         print(i)
--- data_processing/dataUtils.py
+++ data_processing/dataUtils.py
@@ -1,4 +1,10 @@
-# Provides utilities for processing data, including 9
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+
+# Provides utilities for processing data, including.
+# Authored: 11/17/2019
 
 
 # Writes the features to a CSV file.
@@ -6,10 +12,38 @@
 # Parameters  -
 # featureVectorsArray: array of arrays with each second level array representing a feature vectors
 # fileName: the name given to the file, this does not include the extension or directory, since that is assigned
+# labels: the label for each column, in order from left to right. Taken in as a comma seperate list of strings:
+#   ie. ['apples', 'orange', 'bananas']
 # saves the file under the featureSets directory
-def generateFeatureCsv(featureVectorsArray, fileName):
+def generateFeatureCsv(featureVectorsArray, fileName, labels):
     fileName = "../feature_sets/" + fileName + ".csv"
     featureVectorsCsv = open(fileName, "w+")
+    featureVectorsCsv.write(cleanListString(labels) + '\n')
     for vector in featureVectorsArray:
-        stringifiedVector = str(vector).replace("[", "").replace("]", "")
+        stringifiedVector = cleanListString(vector)
         featureVectorsCsv.write(stringifiedVector + '\n')
+
+# Loads a CSV file as a Pandas dataframe
+def retreiveDataSet(path):
+    data = pd.read_csv(path)
+    print(data)
+    return data
+
+# Turns a list into a comma seperated string
+# Parameters:
+# list: a list type
+# returns: a string of comma seperated values
+def cleanListString(list):
+    return str(list).replace("[", "").replace("]", "")
+
+# Plots data using seaborn and matplotlib. Wraps this utilitiy to a generalized method.
+def plotData(xLabel, yLabel, res):
+    sb.lmplot(xLabel, yLabel,
+               data=res,
+               fit_reg=False,
+               hue="clusters")
+
+     # Scatter plot
+    plt.title("Clusters " + xLabel + " vs " + yLabel)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
